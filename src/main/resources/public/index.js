504639e2ee88357bf3b19504344061e84f4ccc9d
unknown <sergio.lopez-v@labinfo.is.escuelaing.edu.co>
2024-03-09 12:05:45 -0500

    v5

---
+++ src/main/resources/public/index.js
@@ -0,0 +1,12 @@
+// index.js
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './App.css'; // Puedes crear este archivo para estilos globales si lo necesitas.
+import App from './App';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
